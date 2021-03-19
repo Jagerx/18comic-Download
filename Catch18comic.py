5e1fe23e2c13db773d7068a962c0843b9606b010
山远水济 <mao.youjia@gmail.com>
2021-03-19 17:00:03 +0800

    修复图片缺行的bug
    转换图片前，重新计算了最后底部图块的高度。

--- Catch18comic.py
+++ Catch18comic.py
@@ -139,24 +139,18 @@ def convertImg(img_url):
         img = Image.open(img_url)
         img_size = img.size
         img_crop_size = int(img_size[1] / 10)
-        img_crop_size_last = (img_size[1] / 10) - img_crop_size  # 解决图片height不能被10整除导致拼接后下方黑条
-        img_crop_size_last = round(img_crop_size_last, 1)
-        if img_crop_size_last > 0:  # 只有无法整除时才将新建图片进行画布纵向减小
-            img_crop_size_last_sum = int(img_crop_size_last * 10)
-        else:
-            img_crop_size_last_sum = 0
         img_width = int(img_size[0])
         img_block_list = [] #定义一个列表用来存切割后图片
-        for img_count in range(10):
+        for img_count in range(9):  #处理前9个规则高度的块
             img_crop_box = (0, img_crop_size*img_count, img_width, img_crop_size*(img_count+1))
             img_crop_area = img.crop(img_crop_box)
-            img_block_list.append(img_crop_area)
-        img_new = Image.new('RGB', (img_size[0], img_size[1]-img_crop_size_last_sum))
-        count = 0
-        for img_block in reversed(img_block_list):
-            img_new.paste(img_block, (0, count*img_crop_size))
-            count += 1
-        # img_new.show() # 调试显示转化后的图片
+            img_crop_yposition = img_size[1] - img_crop_size * (img_count+1)
+            img_block_list.append((img_crop_area,img_crop_yposition)) #存为(切前范围、未来y位置)的元组
+        img_block_list.append((img.crop((0, img_crop_size*9, img_width, img_size[1])), 0))  #单独处理末尾异常高度块
+        img_new = Image.new('RGB', (img_size[0], img_size[1]))
+        for img_block,yposition in img_block_list : 
+            img_new.paste(img_block, (0, yposition)) 
+        #img_new.show() # 调试显示转化后的图片
         img_new.save(img_url)
 
 def get_url_list2(url): #原get_url_list方法采用编号推算，对编号断层没有办法处理，这是新的试验办法
